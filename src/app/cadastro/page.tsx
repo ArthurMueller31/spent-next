cac3514064350d22590d82ccbff90551ffefe723
Arthur <arthurmueller31@gmail.com>
2025-01-10 14:46:22 -0300

    Finished UI for login page and added responsive design

--- src/app/cadastro/page.tsx
+++ src/app/cadastro/page.tsx
@@ -1,8 +1,10 @@
+import Navbar from "../_components/LoginAndSignUp/Navbar";
 import SignUp from "../_components/LoginAndSignUp/SignUp";
 
 export default function LoginAndSignUp() {
   return (
     <>
+      <Navbar />
       <SignUp />
     </>
   );
