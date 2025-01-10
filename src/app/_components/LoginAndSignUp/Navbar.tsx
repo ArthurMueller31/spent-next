d24a1b87e269f087d48862aeca89054e71a37443
Arthur <arthurmueller31@gmail.com>
2025-01-10 14:18:09 -0300

    Implemented UI for sign up page

---
+++ src/app/_components/LoginAndSignUp/Navbar.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+      <nav className="bg-darkerCustomColor w-screen h-16 flex items-center justify-between font-raleway overflow-hidden">
+        <div>
+          <Link href={"/"} className="p-10 flex flex-row items-center">
+            <Image
+              src={"/spent-logo1.png"}
+              width={50}
+              height={50}
+              alt="spent logo"
+              className="mr-1"
+              unoptimized={true}
+              priority
+            />
+            <h1 className="text-white text-xl">SPENT</h1>
+          </Link>
+        </div>
+        <span className="text-white p-10">
+          <Link
+            href={"/sobre"}
+            className="hover:text-gray-300 transition ease-in-out duration-250 text-2xl"
+          >
+            Sobre
+          </Link>
+        </span>
+      </nav>
+  );
+}
