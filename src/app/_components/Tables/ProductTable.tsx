4099ac56cea609483657705e335903b6eae8261f
Arthur <arthurmueller31@gmail.com>
2025-01-29 14:27:40 -0300

    Improve user navigation using icons and titles on the table

--- src/app/_components/Tables/ProductTable.tsx
+++ src/app/_components/Tables/ProductTable.tsx
@@ -1,10 +1,11 @@
 "use client";
 
-import { collection, getDocs } from "firebase/firestore";
+import { collection, deleteDoc, doc, getDocs } from "firebase/firestore";
 import { firestore, auth } from "../../../../firebase/firebase";
 import React, { useEffect, useState } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 import useSidebarStore from "../Navigation/Sidebar/sidebarStore";
+import Image from "next/image";
 
 type Purchase = {
   id?: string;
@@ -112,6 +113,37 @@ export default function ProductTable() {
     fetchData();
   }, [userId, setTotalSpent]);
 
+  const handlePurchaseEdit = async (/* purchaseId: string */) => {
+    return;
+  };
+
+  const handlePurchaseDelete = async (purchaseId: string) => {
+    if (!userId) return;
+
+    const confirmDelete = window.confirm(
+      "Tem certeza de que deseja excluir esta compra?"
+    );
+    if (!confirmDelete) return;
+
+    try {
+      const purchaseRef = doc(
+        firestore,
+        `users/${userId}/purchases/${purchaseId}`
+      );
+
+      await deleteDoc(purchaseRef);
+
+      setPurchases((prevPurchases) =>
+        prevPurchases.filter((p) => p.id !== purchaseId)
+      );
+
+      alert("Compra excluída com sucesso.");
+    } catch (error) {
+      console.log("erro ao excluir", error);
+      alert("Erro ao excluir a compra, tente novamente.");
+    }
+  };
+
   return (
     <>
       <div className="flex h-screen font-raleway tracking-wide">
@@ -120,48 +152,87 @@ export default function ProductTable() {
             {userId && (
               <table className="w-full border border-gray-300 text-sm text-left rounded-lg">
                 <thead className="bg-darkerCustomColor">
-                  <tr className="flex justify-around text-white">
+                  <tr className="flex justify-around text-white text-base">
                     <th className="flex-1 p-3 border-b text-center">
                       Local da Compra
                     </th>
                     <th className="flex-1 p-3 border-b text-center">
                       Quantidade Total de Itens
                     </th>
-                    <th className="flex-1 p-3 border-b text-center">
+                    <th className="flex-1 p-3 border-b text-center ">
                       Preço Total
                     </th>
                     <th className="flex-1 p-3 border-b text-center">
                       Dia da Compra
                     </th>
+                    <th className="flex-1 p-3 border-b text-center">Ações</th>
                   </tr>
                 </thead>
                 <tbody>
                   {purchases.map((purchase) => (
                     <React.Fragment key={purchase.id}>
                       {/* Linha clicável da compra */}
-                      <tr
-                        className="flex justify-around hover:bg-gray-50 cursor-pointer"
-                        onClick={() =>
-                          setExpandedPurchase(
-                            expandedPurchase === purchase.id
-                              ? null
-                              : purchase.id!
-                          )
-                        }
-                      >
-                        <td className="flex-1 p-3 border-b text-center">
+                      <tr className="flex justify-around hover:bg-gray-50 cursor-pointer">
+                        <td className="flex-1 p-3 border-b text-center font-hostGrotesk font-bold flex items-center justify-center text-lg">
                           {purchase.establishment}
                         </td>
-                        <td className="flex-1 p-3 border-b text-center font-hostGrotesk font-light">
+                        <td className="flex-1 p-3 border-b text-center font-hostGrotesk font-bold flex items-center justify-center text-lg">
                           {calculateTotalItems(purchase.items)}
                         </td>
-                        <td className="flex-1 p-3 border-b text-center font-hostGrotesk font-light">
+                        <td className="flex-1 p-3 border-b text-center font-hostGrotesk font-bold flex items-center justify-center text-lg">
                           {formatCurrencyToBRL(
                             calculateTotalPrice(purchase.items)
                           )}
                         </td>
-                        <td className="flex-1 p-3 border-b text-center font-hostGrotesk font-light">
+                        <td className="flex-1 p-3 border-b text-center font-hostGrotesk font-bold flex items-center justify-center text-lg">
                           {formatDate(purchase.purchaseDate)}
+                        </td>
+                        <td className="flex-1 p-3 border-b text-center font-hostGrotesk font-bold flex items-center justify-center text-lg">
+                          <button
+                            className="mx-1 px-2 hover:bg-gray-300 transition duration-200 rounded-xl"
+                            title="Expandir"
+                            onClick={() =>
+                              setExpandedPurchase(
+                                expandedPurchase === purchase.id
+                                  ? null
+                                  : purchase.id!
+                              )
+                            }
+                          >
+                            <Image
+                              className="m-1"
+                              src={"./icons/expand.svg"}
+                              alt="expand-invoice-icon"
+                              width={30}
+                              height={30}
+                            />
+                          </button>
+                          <button
+                            className="mx-1 px-2 hover:bg-gray-300 transition duration-200 rounded-xl"
+                            title="Editar"
+                            onClick={() => handlePurchaseEdit}
+                          >
+                            <Image
+                              className="m-1"
+                              src={"./icons/edit.svg"}
+                              alt="edit-icon"
+                              width={30}
+                              height={30}
+                            />
+                          </button>
+                          <button
+                            className="mx-1 px-2 hover:bg-red-600 transition duration-200 rounded-xl"
+                            title="Excluir"
+                            onClick={() => handlePurchaseDelete(purchase.id!)}
+                          >
+                            <Image
+                              className="m-1"
+                              src={"./icons/delete.svg"}
+                              alt="delete-bin-icon"
+                              width={30}
+                              height={30}
+                            />
+                          </button>
                         </td>
                       </tr>
 
@@ -171,7 +242,7 @@ export default function ProductTable() {
                           <td colSpan={4} className="p-3 bg-gray-50 border-b">
                             <table className="w-full text-sm border border-gray-300 mt-2 rounded-lg">
                               <thead className="bg-darkerCustomColor text-white">
-                                <tr>
+                                <tr className="text-base">
                                   <th className="p-2 border-b text-center">
                                     Nome do Item
                                   </th>
@@ -187,15 +258,15 @@ export default function ProductTable() {
                                 {purchase.items.map((item, index) => (
                                   <tr
                                     key={`${purchase.id}-${index}`}
-                                    className="text-center"
+                                    className="text-center text-base font-medium"
                                   >
                                     <td className="p-2 border-b">
                                       {item.name}
                                     </td>
-                                    <td className="p-2 border-b font-hostGrotesk font-light">
+                                    <td className="p-2 border-b font-hostGrotesk font-mediu">
                                       {item.quantity}
                                     </td>
-                                    <td className="p-2 border-b font-hostGrotesk font-light">
+                                    <td className="p-2 border-b font-hostGrotesk">
                                       {formatCurrencyToBRL(
                                         standardPriceFormat(item.price)
                                       )}
